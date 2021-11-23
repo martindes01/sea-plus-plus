a5c9d2dbeb2540da411896dd31e5b658a41580a9
Martin de Spirlet <martindes01@gmail.com>
2021-11-23 17:28:42 +0000

    Add loop control structure
    - Select appropriate command codes for thrust and steering.
    - Set up a switch statement to handle each command.

--- main.ino
+++ main.ino
@@ -2,13 +2,16 @@
 
 constexpr uint8_t irReceiverPin{ 3 };
 
-constexpr unsigned long baudRate{ 9600UL };
+constexpr uint32_t rudderCentreCommand{ 0x00FF02FDU };
+constexpr uint32_t rudderPortCommand{ 0x00FF22DDU };
+constexpr uint32_t rudderStarboardCommand{ 0x00FFC23DU };
+constexpr uint32_t throttleDecreaseCommand{ 0x00FFA857U };
+constexpr uint32_t throttleIncreaseCommand{ 0x00FF629DU };
+
 constexpr unsigned long loopDelay{ 50UL };
 
 void setup()
 {
-  Serial.begin(baudRate);
-
   IrReceiver.begin(irReceiverPin, ENABLE_LED_FEEDBACK);
 }
 
@@ -16,7 +19,23 @@ void loop()
 {
   if (IrReceiver.decode())
   {
-    IrReceiver.printIRResultShort(&Serial);
+    switch (IrReceiver.decodedIRData.decodedRawData)
+    {
+    case rudderCentreCommand:
+      break;
+
+    case rudderPortCommand:
+      break;
+
+    case rudderStarboardCommand:
+      break;
+
+    case throttleDecreaseCommand:
+      break;
+
+    case throttleIncreaseCommand:
+      break;
+    }
 
     IrReceiver.resume();
   }
