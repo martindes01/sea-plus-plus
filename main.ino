4ba18c666e09142ef7bc033c6f5ccd42376f3822
Martin de Spirlet <martindes01@gmail.com>
2021-11-24 16:32:24 +0000

    Set up rudder servo
    - Include the 'Servo.h' header file.
    - Increase the rudder servo angle to turn to port.
    - Decrease the rudder servo angle to turn to starboard.

--- main.ino
+++ main.ino
@@ -1,6 +1,8 @@
 #include <IRremote.h>
+#include <Servo.h>
 
 constexpr uint8_t irReceiverPin{ 3 };
+constexpr uint8_t servoPin{ 9 };
 
 constexpr uint32_t rudderCentreCommand{ 0x00FF02FDU };
 constexpr uint32_t rudderPortCommand{ 0x00FF22DDU };
@@ -9,10 +11,19 @@ constexpr uint32_t throttleDecreaseCommand{ 0x00FFA857U };
 constexpr uint32_t throttleIncreaseCommand{ 0x00FF629DU };
 
 constexpr unsigned long loopDelay{ 50UL };
+constexpr int rudderCentre{ 90 };
+constexpr int rudderStep{ 5 };
+
+static Servo s_rudder{};
+
+static int s_rudderAngle = rudderCentre;
 
 void setup()
 {
   IrReceiver.begin(irReceiverPin, ENABLE_LED_FEEDBACK);
+
+  s_rudder.attach(servoPin);
+  s_rudder.write(s_rudderAngle);
 }
 
 void loop()
@@ -22,12 +33,16 @@ void loop()
     switch (IrReceiver.decodedIRData.decodedRawData)
     {
     case rudderCentreCommand:
+      s_rudder.write(rudderCentre);
+      s_rudderAngle = rudderCentre;
       break;
 
     case rudderPortCommand:
+      s_rudder.write(s_rudderAngle += rudderStep);
       break;
 
     case rudderStarboardCommand:
+      s_rudder.write(s_rudderAngle -= rudderStep);
       break;
 
     case throttleDecreaseCommand:
